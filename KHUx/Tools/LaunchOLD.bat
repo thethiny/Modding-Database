4087ddd6400c4a62af3c1cf07876183bf5b4c8e2
thethiny <thin_boy_123@yahoo.com>
2016-09-01 17:57:32 +0400

    Fix for Launch.bat
    Added the ability to convert non dat files and files with spaces.

--- KHUx/Tools/LaunchOLD.bat
+++ KHUx/Tools/LaunchOLD.bat
@@ -4,17 +4,16 @@ md DATS 2>nul
 del pngList.txt 2>nul
 cd Input
 for %%f in (*.png) do (
-md "..\Output\%%f_out" 2>nul
+md ..\Output\%%f_out 2>nul
 echo %%f%>>..\pngList.txt
 cls
 echo %%f
 )
 cls
 for %%f in (*.png) do (
-..\offzip -a "%%f" "..\Output\%%f_out"
-ren "..\Output\%%f_out\*" "%%f.dat" >nul
-move "..\Output\%%f_out\*" "..\DATS\" >nul
-
+..\offzip -a -z -15 -Q %%f ..\Output\%%f_out
+ren ..\Output\%%f_out\*.dat %%f.dat >nul
+move ..\Output\%%f_out\*.dat ..\DATS\ >nul
 )
 cd ..\
 rmdir /s /q Output
